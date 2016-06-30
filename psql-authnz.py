4da4642414ec407a5cbbca80e9394b022f7ff852
Tim Anderegg <timothy.anderegg@cfpb.gov>
2016-06-30 10:19:09 -0400

    Added basic logging and error handling.

--- psql-authnz.py
+++ psql-authnz.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python
 
-from pprint import pprint
-import os
+import pprint
+import os, sys
+
+import logging
+
 import ldap
 
+
 if __name__ == '__main__':
-    conn = ldap.initialize('ldap://10.0.1.127:389')
+    log_level   = os.getenv("PSQL_AUTHNZ_LOG_LEVEL", "warn")
+    username    = os.getenv("PSQL_AUTHNZ_LDAP_USERNAME", None)
+    password    = os.getenv("PSQL_AUTHNZ_LDAP_PASSWORD", None)
+    domain      = os.getenv("PSQL_AUTHNZ_LDAP_DOMAIN", "dc=test,dc=dev")
+    group_ou    = os.getenv("PSQL_AUTHNZ_GROUP_OU", "ou=Groups")
+    user_ou     = os.getenv("PSQL_AUTHNZ_USER_OU", "ou=Users")
 
-    username = os.getenv("LDAP_USERNAME", None)
-    password  = os.getenv("LDAP_PASSWORD", None)
+    LOG_FORMAT = "%(asctime)-15s PSQL-AUTHNZ: %(message)s"
+    logging.basicConfig(format=LOG_FORMAT,level=getattr(logging, log_level.upper()))
 
-    if username and password:
-        auth_tokens = ldap.sasl.digest_md5(username, password)
-        conn.sasl_interactive_bind_s("", auth_tokens)
-    else:
-        conn.simple_bind_s()
+    try:
+        conn = ldap.initialize('ldap://10.0.1.127:389')
 
-    domain = "dc=test,dc=dev"
-    group_ou = "ou=Groups"
-    group_search_base = group_ou + "," + domain
+        # If a username and password is provided, we assume
+        # SASL's DIGESTMD5 authentication method.
+        if username and password:
+            auth_tokens = ldap.sasl.digest_md5(username, password)
+            conn.sasl_interactive_bind_s("", auth_tokens)
+        else:
+            conn.simple_bind_s()
+    except ldap.LDAPError, e:
+        logging.error(e)
+        sys.exit(1)
 
-    data_access_groups = conn.search_s(group_search_base, ldap.SCOPE_SUBTREE, "(objectCLass=groupOfNames)")
-    print "Data access groups:"
-    pprint(data_access_groups)
+    try:
+        data_access_groups_search_base = group_ou + "," + domain
+        data_access_groups = conn.search_s(data_access_groups_search_base, ldap.SCOPE_SUBTREE, "(objectCLass=groupOfNames)")
+    except ldap.LDAPError, e:
+        logging.error(e)
+        sys.exit(1)
+
+    logging.debug("Data access groups: \n{0}".format(pprint.pformat(data_access_groups)))
 
     for group in data_access_groups:
-        print "Group: " + group[1]['cn'][0] + " has members: "
-        print group[1]['member']
+        logging.debug("Group '{0}' has members: {1}".format(group[1]['cn'][0], group[1]['member']))
 
-    user_ou = "ou=Users"
-    user_search_base = user_ou + "," + domain
+    # TODO: Remove this section, just for testing purposes
+    try:
+        user_search_base = user_ou + "," + domain
+        users = conn.search_s(user_search_base, ldap.SCOPE_SUBTREE, "(objectClass=*)")
+    except ldap.LDAPError, e:
+        logging.error(e)
+        sys.exit(1)
 
-    results = conn.search_s(user_search_base, ldap.SCOPE_SUBTREE, "(objectClass=*)")
-    print "Users:"
-    pprint(results)
+    logging.debug("Users: \n{0}".format(pprint.pformat(users)))
+    sys.exit(0)
