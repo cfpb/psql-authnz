4e63143f048a200668c91c831300d1a703ebd8bb
Tim Anderegg <timothy.anderegg@cfpb.gov>
2016-11-21 12:57:47 -0500

    Fixed improper looping.

--- psql_authnz.py
+++ psql_authnz.py
@@ -3,11 +3,5 @@ import os, sys
 import time
 from psql_authnz.psql_authnz import main
 
-psql_period = os.getenv("PSQL_AUTHNZ_PERIOD", 300)
-
 if __name__ == '__main__':
-    while True:
-        exit_code = main()
-        if exit_code != 0:
-            break
-        time.sleep(psql_period)
+    sys.exit(main())
